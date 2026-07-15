8e96ae46d23bef8fc053c9f2a754cfff5104347c
kndssw-ops <kndssw@gmail.com>
2026-07-15 14:20:13 +0900

    에러0_3

---
+++ 03_error.py
@@ -0,0 +1,32 @@
+# ===================
+# print("=====NameError 만들기=====")
+
+# print(NameError 만들기) # SyntaxError 발생 (띄어쓰기가 있어서 변수 2개를 쉼표 없이 작성했다고 추정)
+# print(NameError만들기)  # NameError 발생
+# # 코드는 위에서 아래로 실해되기 떄문에 위에서 에러 발생하면 그 이후 코드 실행 X
+# print("NameError 만들기")  # 따옴표로 감싸주면 에러 발생 X
+
+# ==========================
+# print("=====SyntaxError 만들기=====")
+
+# # print("온도) # 따옴표를 안닫음
+# # print("진동" # 괄호를 안닫음
+
+# print("온도")
+# print("진동")
+
+# print("온도", 75) # 온도에 따옴표를 감싸지 않음
+# print("진동", 2.3) # 괄호를 닫으면서 마치지 않음
+# print("압력:", 4.0) #
+
+print("===== 1번 설비 점검 =====")
+print("온도(°C):", 82)
+print("온도 상승량:", 93 - 82)
+
+if
+for
+def
+
+if True
+temp
+
